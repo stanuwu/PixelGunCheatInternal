4ccdf7362b3017d8ff7ff8396c0b0d11da033bfd
Mezo <47868851+Mezo-hx@users.noreply.github.com>
2024-04-18 00:06:11 -0400

    asd
    asd

---
+++ PixelGunCheat/Cheat/Hooks/Hooks.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <list>
+#include <xstring>
+
+#include "../Module/ModuleBase.h"
+
+class Hooks
+{
+public:
+    static uint64_t tick;
+    static std::list<void*> player_list;
+    static void* our_player;
+    static void* main_camera;
+    static void* aimed_pos;
+    static void dump_item_records();
+    static void draw_all();
+    static std::string get_player_name(void* player_move_c);
+    static void* get_player_transform(void* player);
+    static bool is_player_enemy(void* player);
+    void load();
+    void unload();
+    static uintptr_t GameBase;
+    static uintptr_t GameAssembly;
+    static uintptr_t UnityPlayer;
+    static ModuleBase* fov_changer_module;
+    static std::list<ModuleBase*> on_imgui_draw_modules;
+};
---
+++ PixelGunCheat/Cheat/Module/Impl/ModuleUnlockWeapons.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <stdbool.h>
+
+#include "../ModuleBase.h"
+#include "../../Gui/imgui_hooker.h"
+
+static BKCDropdown __weapon_list = { "Weapon Selector", weapons_names[0], weapons_names, "THIS IS A VOLATILE MODULE, AND HAS A POSSIBILITY TO BAN DEPENDING ON HOW YOU USE IT", true };
+static BKCCheckbox __unlock_weapons_dev = {"Spoof Developer", true, "Tries to add the weapon through developer mode, less consistent but safer."};
+static BKCCheckbox __unlock_weapons_all = {"Unlock All", false, "WARNING, THIS ONLY WORKS FROM THE LOTTERY SUPERCHEST AND WILL CRASH THE GAME, BUT YOU SHOULD HAVE ALL THE WEAPONS AFTERWARDS"};
+static BKCModule __unlock_weapons = { "Weapon Unlock", EXPLOIT, 0x0, ImGuiKey_None, false, { &__weapon_list, &__unlock_weapons_dev, &__unlock_weapons_all } };
+
+class ModuleUnlockWeapons : ModuleBase
+{
+public:
+    ModuleUnlockWeapons() : ModuleBase(&__unlock_weapons) {}
+    
+    void do_module(void* arg) override
+    {
+        
+    }
+
+    bool all()
+    {
+        return __unlock_weapons_all.enabled;
+    }
+
+    void lock()
+    {
+        __unlock_weapons.enabled = false;
+    }
+
+    std::string to_unlock()
+    {
+        return __weapon_list.current_value;
+    }
+
+    bool dev()
+    {
+        return __unlock_weapons_dev.enabled;
+    }
+};
